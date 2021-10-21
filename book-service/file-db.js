ffb15f475cc4ac28598749b236c9f9a63ed34fb8
vijay <uvijaysimha@gmail.com>
2021-10-21 12:48:00 +0530

    added seed data

--- book-service/file-db.js
+++ book-service/file-db.js
@@ -2,32 +2,88 @@ let uuid = require('uuid/v1');
 let books = [];
 
 
-books.push({
+books.push(
+    {
+        id: uuid(),
+        isbn: "978-3-548-26308-3",
+        title: "Es",
+        author: "Stephen King",
+        publisher: "Ullstein",
+        year: 2005,
+        genre: "Horror",
+        condition: "New",
+        available: true
+    },
+    {
+        id: uuid(),
+        isbn: "978-3-548-26308-3",
+        title: "Schnelles Denken, langsames Denken",
+        author: "Daniel Kahneman",
+        publisher: "Penguin Verlag",
+        year: 2016,
+        genre: "Sachbuch",
+        condition: "New",
+        available: true
+    },
+    {
+        id: uuid(),
+        "isbn": "19-420-5805",
+        "title": "Grin Without a Cat, A (Fond de l'air est rouge, Le)",
+        "author": "Maéna",
+        "publisher": "Leptosiphon floribundum (A. Gray) J.M. Porter & L.A. Johnson ssp. glaber (R. Patt.) J.M. Porter & L.A. Johnson",
+        "year": 2006,
+        "genre": "Documentary",
+        "available": false,
+        condition: "New",
+    }, {
     id: uuid(),
-    isbn: "978-3-548-26308-3",
-    title: "Es",
-    author: "Stephen King",
-    publisher: "Ullstein",
-    year: 2005,
-    genre: "Horror",
+    "isbn": "70-591-3166",
+    "title": "The Trans-Atlantic Mystery",
+    "author": "Rébecca",
+    "publisher": "Lupinus albicaulis Douglas",
+    "year": 2003,
+    "genre": "(no genres listed)",
+    "available": false,
     condition: "New",
-    available: true
-},
-{
+}, {
     id: uuid(),
-    isbn: "978-3-548-26308-3",
-    title: "Schnelles Denken, langsames Denken",
-    author: "Daniel Kahneman",
-    publisher: "Penguin Verlag",
-    year: 2016,
-    genre: "Sachbuch",
+    "isbn": "15-778-8390",
+    "title": "Song of the South",
+    "author": "Céline",
+    "publisher": "Agrostis canina L.",
+    "year": 2004,
+    "genre": "Adventure|Animation|Children|Musical",
+    "available": true,
     condition: "New",
-    available: true
-});
+}, {
+    id: uuid(),
+    "isbn": "43-158-6850",
+    "title": "Murder, My Sweet",
+    "author": "Dafnée",
+    "publisher": "Sisyrinchium langloisii Greene",
+    "year": 1998,
+    "genre": "Crime|Film-Noir|Thriller",
+    "available": false,
+    condition: "New",
+}, {
+    id: uuid(),
+    "isbn": "88-731-1849",
+    "title": "Bass Ackwards",
+    "author": "Clélia",
+    "publisher": "Mimulus lewisii Pursh",
+    "year": 1998,
+    "genre": "Drama",
+    "available": false,
+    condition: "New",
+}
+
+
+);
 
 const addBook = book => {
     const id = uuid();
-    let newBook = { ...book,
+    let newBook = {
+        ...book,
         id: id
     }
     books.push(newBook);
@@ -56,7 +112,7 @@ const getBookById = id => {
 
 const setAvailable = (id) => {
     const index = books.findIndex(book => book.id === id);
-    books[index] = {...books[index], available: !books[index].available};
+    books[index] = { ...books[index], available: !books[index].available };
     return books[index];
 }
 
--- customer-service/file-db.js
+++ customer-service/file-db.js
@@ -2,28 +2,60 @@
 let uuid = require('uuid/v1');
 let customers = [];
 
-customers.push({
+customers.push(
+    {
     id: uuid(),
     firstname: "Max",
     lastname: "Mustermann",
     address: "Musterstr. 12",
     birthday: new Date(1990, 9, 15).getTime()
-},
-{
+    },
+    {
+        id: uuid(),
+        firstname: "Klaus",
+        lastname: "Müller",
+        address: "Hauptstr. 2",
+        birthday: new Date(1965, 3, 21).getTime()
+    },
+    {
+        id: uuid(),
+        "firstname": "May",
+        "lastname": "De Souza",
+        "address": "148 Homewood Alley",
+        "birthday": 1613902719000
+    }, {
     id: uuid(),
-    firstname: "Klaus",
-    lastname: "Müller",
-    address: "Hauptstr. 2",
-    birthday: new Date(1965, 3, 21).getTime()
-}
+    "firstname": "Jeffy",
+    "lastname": "Mobius",
+    "address": "00475 Fair Oaks Crossing",
+    "birthday": 1619492812000
+    }, {
+        id: uuid(),
+        "firstname": "Dalia",
+        "lastname": "Todaro",
+        "address": "35 Upham Crossing",
+        "birthday": 1611726298000
+    }, {
+        id: uuid(),
+        "firstname": "Mitchel",
+        "lastname": "Matteuzzi",
+        "address": "85826 Lotheville Plaza",
+        "birthday": 1627961448000
+    }, {
+        id: uuid(),
+        "firstname": "Brittni",
+        "lastname": "Muzzollo",
+        "address": "127 Donald Avenue",
+        "birthday": 1633779291000
+    }
 );
 
 const addCustomer = (customer) => {
     const id = uuid();
-    let newCustomer = {...customer, id: id}
+    let newCustomer = { ...customer, id: id }
     customers.push(newCustomer);
     return newCustomer;
-} 
+}
 
 const updateCustomer = (customer) => {
     const index = customers.findIndex(element => element.id === customer.id);
